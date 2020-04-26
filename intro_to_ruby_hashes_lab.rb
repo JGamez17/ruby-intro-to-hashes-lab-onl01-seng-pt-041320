391e8d5d13cbb272c965a1a63629a0bbdf3af536
Jessica Gamez <jpgamez1789@icloud.com>
2020-04-26 21:17:57 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -46,4 +46,6 @@ def monopoly_with_fourth_tier
   monopoly = {:railroads => {
     :pieces => 4,
     :names => {"mortgage_value" => "100"},
-    :reading_railraod =>
+    :reading_railraod => {"mortgage_value" => "100"},
+    :pennsylvania_railroad => {"mortgage_value" => "100"},
+    :b_and_o_railroad => {"mortgage_value" => "100"}
